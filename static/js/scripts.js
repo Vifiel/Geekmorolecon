3e0eeed50eb4c54c4ee95d25397a1751d089e660
Vifiel <ibaklanov.696@gmail.com>
2025-05-15 20:48:13 +0300

    Добавление окошка формы и гитигнора

---
+++ static/js/scripts.js
@@ -0,0 +1,11 @@
+function openForm() {
+  document.getElementById("regForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("regForm").style.display = "none";
+}
+
+function sendInfo() {
+    console.log("aaaaa");
+}
