9f2a70abc5c38975f60c3818cbac99e31fdedbac
Vifiel <ibaklanov.696@gmail.com>
2025-06-23 21:05:14 +0300

    Добавление получения данных из формы регистрации

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -7,6 +7,13 @@ app = Flask("Reg")
 @app.route("/")
 def main():
     return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form['email']
+    password = request.form['pswd']
+    print(password, email)
+    return redirect(url_for("main"))
 
 if __name__ == "__main__":
     app.run()
