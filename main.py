a11323f2ff743c46f8b508d01f3f2da9cae1aac9
Vifiel <ibaklanov.696@gmail.com>
2025-06-24 14:07:32 +0300

    Исправление ошибок в регистрации

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import hashlib
+import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("registration-64a55-firebase-adminsdk-fbsvc-e0be139804.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
