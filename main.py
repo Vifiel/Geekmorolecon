6e478197e3427c5ca271b92ba56b8c3b89e46f1b
Vifiel <ibaklanov.696@gmail.com>
2025-06-24 12:42:52 +0300

    Добавлены структурирование получаемых от пользователя данных, хэширование пароля

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import hashlib
 
 app = Flask("Reg")
+
+class User:
+    def __init__(self, email, password):
+        self.password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+        self.email = email
+    
+
+class UserData(User):
+    def __init__(self, email, password, name):
+        super().__init__(email, password)
+        self.name = name
+
+
+
+
 
 @app.route("/")
 def main():
@@ -10,9 +26,8 @@ def main():
 
 @app.route("/register", methods=["POST"])
 def register():
-    email = request.form['email']
-    password = request.form['pswd']
-    print(password, email)
+    form_data = request.form.to_dict()
+    user = UserData(form_data['email'], form_data['pswd'], form_data['name'])
     return redirect(url_for("main"))
 
 if __name__ == "__main__":
