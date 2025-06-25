8f8584a6d5abc08257c522d33c4e5ed6bd525237
l1nxGD <useer095698@gmail.com>
2025-06-25 13:23:49 +0300

    Довлена поддержка сессии пользователя

--- back/user.py
+++ back/user.py
@@ -1,10 +1,25 @@
 import hashlib
+from flask_login import UserMixin
 from database import init_db
 
-class User:
+class User(UserMixin):
     def __init__(self, email, password):
         self.password = hashlib.sha256(password.encode("utf-8")).hexdigest()
         self.email = email
+
+    @staticmethod
+    def get(email):
+        # Получаем пользователя по email (который является ID)
+        user_ref = init_db.db.collection('users').document(email)
+        user_data = user_ref.get()
+
+        if user_data.exists:
+            return User(
+                email=email,
+                password=user_data.get('password')
+            )
+        return None
+
 
 class UserData(User):
     def __init__(self, data):
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_login import login_user, logout_user, login_required, current_user, LoginManager
 from back.user import User, UserData
 from back.section import Section
 from database import init_db
 
 app = Flask("Reg")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 current_user = None
 
@@ -35,7 +39,7 @@ def enter():
     is_exist = user_data.exists 
     is_pass_match = user_data.get("password") == current_user.password
     respond = jsonify({"exists": user_data.exists, "passMatch": is_pass_match})
-    
+
     if not is_exist:
         current_user = None
         return  respond
@@ -43,6 +47,7 @@ def enter():
         current_user = None
         return respond
     else:
+        login_user(current_user)
         return respond
 
 @app.route("/createSection", methods=["POST"])
@@ -50,9 +55,45 @@ def createSection():
     form_data = request.form.to_dict()
     print(form_data)
     current_section = Section(form_data)
-    current_section.post()
+#    current_section.post()
+
+    if not current_section.isExist():
+        current_section.post()
+        print('данные добавлены')
+    else:
+        current_section.update(form_data)
+        print('данные обновлены')
 
     return redirect(url_for("main"))
+
+@app.route("/entryToSection", methods=["POST"])
+@login_required
+def entryToSection():
+    form_data = request.form.to_dict()
+    print(form_data)
+    current_section = Section(form_data)
+#    current_section.post()
+
+    if not current_section.isExist():
+        email = current_user.email
+        current_section.post(email)
+
+        print('пользователь добавлен')
+
+    return redirect(url_for("main"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main"))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # user_id - это email
+    return User.get(user_id)
+
 
 if __name__ == "__main__":
     app.run()
