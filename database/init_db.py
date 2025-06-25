f75d21548c63067cf33a2ebd4753a85d339e30ee
Vifiel <ibaklanov.696@gmail.com>
2025-06-25 19:26:38 +0300

    Изменение инициализации бд для работы на railway

--- database/init_db.py
+++ database/init_db.py
@@ -1,9 +1,17 @@
 import firebase_admin
 from firebase_admin import firestore
 import os
+import tempfile
 
 firebase_key = os.getenv("FIREBASE_KEY")
-cred = firebase_admin.credentials.Certificate(firebase_key)
+
+with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as temp:
+    temp.write(firebase_creds_str.encode())
+    temp_path = temp.name
+
+cred = firebase_admin.credentials.Certificate(temp_path)
 firebase_admin.initialize_app(cred)
+
+os.remove(temp_path)
 
 db = firestore.client()
