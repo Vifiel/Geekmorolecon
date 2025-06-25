a3082f1444b0c9acef4d8a4396d627858d8411db
Vifiel <ibaklanov.696@gmail.com>
2025-06-25 22:36:05 +0300

    Переписана инициализация бд для более корректной работы

--- database/init_db.py
+++ database/init_db.py
@@ -6,18 +6,19 @@ from dotenv import load_dotenv
 
 firebase_key = os.getenv("FIREBASE_KEY")
 
-if not firebase_key:
-    load_dotenv() 
+if firebase_key:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as temp:
+        temp.write(firebase_key.encode())
+        temp_path = temp.name
+        key = temp_path
+    cred = firebase_admin.credentials.Certificate(temp_path)
+    firebase_admin.initialize_app(cred)
 
-    firebase_key = os.getenv("FIREBASE_KEY")
+    os.remove(temp_path)
+else:
+    key = "registration-64a55-firebase-adminsdk-fbsvc-8015080d81.json"
 
-with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as temp:
-    temp.write(firebase_key.encode())
-    temp_path = temp.name
-
-cred = firebase_admin.credentials.Certificate(temp_path)
-firebase_admin.initialize_app(cred)
-
-os.remove(temp_path)
+    cred = firebase_admin.credentials.Certificate(key)
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
