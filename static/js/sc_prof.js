0a87a0d92a7797a71eb5a08b9b8a7d628af952c0
Vifiel <ibaklanov.696@gmail.com>
2025-07-03 11:05:54 +0300

    Переписал страницу профиля без extend, считаю, что он там не нужен

---
+++ static/js/sc_prof.js
@@ -0,0 +1,7 @@
+
+function showEditForm(formId) {
+    document.getElementById(formId).style.display = 'block';
+}
+function hideEditForm(formId) {
+    document.getElementById(formId).style.display = 'none';
+}
