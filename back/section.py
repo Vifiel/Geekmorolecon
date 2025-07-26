f0faa97ca68ce849a34263e4a45c7de961decde3
Vifiel <ibaklanov.696@gmail.com>
2025-07-26 15:47:58 +0300

    Исправление багов авторизации

--- back/section.py
+++ back/section.py
@@ -1,4 +1,5 @@
 from database.init_db import db
+import uuid
 
 class Section:
 
@@ -14,19 +15,16 @@ class Section:
         self.master = data['master']
         self.masteClub = data['masterClub']
         self.system = data['system']
+        self.id = str(uuid.uuid4())
 
         self.image = ""
 
     def isExist(self):
-        section_data = db.collection("section").document(self.name).get()
+        section_data = db.collection("section").document(self.id).get()
         return section_data.exists
 
     def post(self):
-        db.collection("section").document(self.name).set(self.data)
+        db.collection("section").document(self.id).set(self.data)
 
     def update(self, new_data):
-        db.collection("section").document(self.name).set(new_data, merge=True)
-
-    def entryUser(self, email):
-        self.data['users'].append(email)
-        db.collection("section").document(self.name).set(self.data, merge=True)
+        db.collection("section").document(self.id).set(new_data, merge=True)
